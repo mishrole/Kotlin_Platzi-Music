04d2954041cbee271ada12cd99026f4aa11f1af4
Mitchell Rodríguez <mitrodle@gmail.com>
2021-09-07 03:30:18 -0500

    [Add]: Viewgroup vs View + Gravity multi-properties

--- app/src/main/java/com/mishrole/platzimusic/MainActivity.kt
+++ app/src/main/java/com/mishrole/platzimusic/MainActivity.kt
@@ -6,6 +6,15 @@ import android.os.Bundle
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_profile)
+
+        // Resources reference
+        R.color.purple_200
+
+        /*
+        * View is an inline element
+        * ViewGroup is a group of inline elements, and those elements inherit properties from the parent
+        * */
     }
 }
